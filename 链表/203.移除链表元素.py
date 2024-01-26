c9b80368021bb8cf837818af60a13d56e1adf63d
hzs <huangzseulgi@163.com>
2024-01-26 15:05:06 +0800

    add leecode 203.移除链表元素

--- 链表/203.移除链表元素.py
+++ 链表/203.移除链表元素.py
@@ -1,5 +1,5 @@
 # @before-stub-for-debug-begin
-from python3problem203 import *
+from tkinter.tix import ListNoteBook
 from typing import *
 # @before-stub-for-debug-end
 
@@ -8,15 +8,32 @@ from typing import *
 #
 # [203] 移除链表元素
 #
-from typing import *
+
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# from tkinter.tix import ListNoteBook
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # 处理空链表的情况
+        if not head:
+            return None
         
-# @lc code=end
+        # 处理头节点就是要删除的情况
+        while head and head.val == val:
+            head = head.next
 
+        cur = head
+        while cur and cur.next:
+            if cur.next.val == val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+
+        return head
+# @lc code=end
